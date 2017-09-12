f4ae685bbdb13f5aa192f0f61ab3f02f9379dddf
lukaslid <lukaslid@gmail.com>
2017-09-12 20:31:15 +0300

    Added order model controller and migrations, edited layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('partials.layout')
+
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col">
+                <h3>YOOOOO</h3>
+                <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+                <p>Back to minus the navbar.</p>
+            </div>
+            <div class="col-6">
+                <h3>YOOOOO</h3>
+                <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+                <p>Back to minus the navbar.</p>
+            </div>
+            <div class="col">
+                <h3>YOOOOO</h3>
+                <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+                <p>Back to minus the navbar.</p>
+            </div>
+        </div>
+    <!-- Begin page content -->
+@endsection
---
+++ resources/views/partials/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+@include('partials.nav')
+
+@yield('content')
+
+@include('partials.footer')
+
+</body>
+</html>
