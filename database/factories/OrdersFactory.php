f451b79c50e2370b46b34af6a6b2f5300960be7a
lukaslid <lukaslid@gmail.com>
2017-09-14 17:17:22 +0300

    Completed db seeding

--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -2,8 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Order::class, function (Faker $faker) {
     return [
-        //
+        'full_name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'number' => $faker->phoneNumber,
+        'amount' => $faker->biasedNumberBetween(1,3)
     ];
 });
