063389896d1f72d1f01a336fb15e67feab3675ae
lukaslid <lukaslid@gmail.com>
2017-09-15 17:57:52 +0300

    fixed mail sender added constants file

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukas
+ * Date: 2017-09-15
+ * Time: 17:49
+ */
+return [
+    'order' => [
+        'order_price' => '400€',
+    ]
+];
--- resources/views/emails/orderReceived.blade.php
+++ resources/views/emails/orderReceived.blade.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
 <body>
     <h1>TODO: Add payment method</h1>
+    <h4>Užsakymas įvykdytas sėkmingai!</h4>
+    <ul>
+        <li>Užsakovas: {{$order->full_name}}, {{$order->email}} </li>
+        <li>Kaina: {{Config::get('constants.order.order_price')}}</li>
+        <li>Užsakymo laikas: {{$order->created_at}}</li>
+    </ul>
 </body>
 </html>
