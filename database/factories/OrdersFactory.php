3a29434d4af691a5d61bd40d918b010ce36f9de4
lukaslid <lukaslid@gmail.com>
2017-09-16 15:58:54 +0300

    default property

--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -7,5 +7,6 @@ $factory->define(App\Order::class, function (Faker $faker) {
         'full_name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'number' => $faker->phoneNumber,
+        'completed' => false
     ];
 });
