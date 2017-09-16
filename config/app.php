b2d2269261acfc09b05f84962458f741a418bbc8
lukaslid <lukaslid@gmail.com>
2017-09-16 15:49:11 +0300

    added url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://shielded-badlands-91770.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
